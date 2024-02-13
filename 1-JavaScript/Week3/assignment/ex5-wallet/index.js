186617099f82796d8384ed096ceaaf299212a8ed
ervandogan12 <ervandogan12@gmail.com>
2024-02-13 15:06:19 +0100

    Finished project for assignment week 3

--- 1-JavaScript/Week3/assignment/ex5-wallet/index.js
+++ 1-JavaScript/Week3/assignment/ex5-wallet/index.js
@@ -14,7 +14,6 @@ function createWallet(name, cash = 0) {
 
   function withdraw(amount) {
     if (cash - amount < 0) {
-      console.log(`Insufficient funds!`);
       return 0;
     }
 
@@ -24,9 +23,9 @@ function createWallet(name, cash = 0) {
 
   function transferInto(wallet, amount) {
     console.log(
-      `Transferring ${eurosFormatter.format(amount)} from ${name} to ${
-        wallet.name
-      }`
+      `--28--Transferring ${eurosFormatter.format(
+        amount
+      )} from ${name} to ${wallet.getName()}`
     );
     const withdrawnAmount = withdraw(amount);
     wallet.deposit(withdrawnAmount);
@@ -77,8 +76,10 @@ const quiz = {
       b: 'cash, name', 
       c: 'amount, this, wallet'
     },
-    answer: undefined,
+    answer: "b"
   },
+
+
   q2: {
     question: 'What is in the Call Stack, from top to bottom?',
     choices: { 
@@ -86,7 +87,7 @@ const quiz = {
       b: 'anonymous, transferInto', 
       c: 'transferInto, anonymous' 
     },
-    answer: undefined,
+    answer:"c"
   },
   q3: {
     question: 'What tooltip appears when hovering over the third debug button?',
@@ -95,7 +96,7 @@ const quiz = {
       b: 'Step out of current function', 
       c: 'Step' 
     },
-    answer: undefined,
+    answer: "a"
   },
   q4: {
     question: 'What is displayed in the console?',
@@ -104,7 +105,7 @@ const quiz = {
       b: 'Transferring € 50,00 from Jack to undefined', 
       c: 'Transferring € 50,00 from Jack to Jane' 
     },
-    answer: undefined,
+    answer:"a",
   },
   q5: {
     question: 'The owner of the wallet with insufficient funds is:',
@@ -113,6 +114,6 @@ const quiz = {
       b: 'Joe', 
       c: 'Jane' 
     },
-    answer: undefined,
+    answer: "c",
   },
 };
