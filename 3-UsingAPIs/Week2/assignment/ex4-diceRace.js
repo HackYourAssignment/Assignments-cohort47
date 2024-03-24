69feae13040d70ee692f789b0b40c2288aa67990
MustafaTRKYLMZ <cana66331@gmail.com>
2024-03-24 17:35:39 +0100

    ex4 completed

--- 3-UsingAPIs/Week2/assignment/ex4-diceRace.js
+++ 3-UsingAPIs/Week2/assignment/ex4-diceRace.js
@@ -15,14 +15,18 @@ const rollDie = require('../../helpers/pokerDiceRoller');
 
 function rollDice() {
   const dice = [1, 2, 3, 4, 5];
-  // TODO complete this function; use Promise.race() and rollDie()
+  const promises = dice.map(() => rollDie());
+  return Promise.race(promises);
 }
 
 // Refactor this function to use async/await and try/catch
-function main() {
-  rollDice()
-    .then((results) => console.log('Resolved!', results))
-    .catch((error) => console.log('Rejected!', error.message));
+async function main() {
+  try {
+    const results = await rollDice();
+    console.log('Resolved!', results);
+  } catch (error) {
+    console.log('Rejected!', error.message);
+  }
 }
 
 // ! Do not change or remove the code below
@@ -30,3 +34,10 @@ if (process.env.NODE_ENV !== 'test') {
   main();
 }
 module.exports = rollDice;
+
+/* 
+
+we are using promise.race. Because =, we are only looking for specific result. 
+When we have this result function have to ve return. 
+Otherwise we have wait all of the result of the function.  
+*/
