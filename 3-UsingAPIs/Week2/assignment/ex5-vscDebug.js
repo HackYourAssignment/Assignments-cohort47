0717310d0f6f3d23dbd771762396ca34598b4fd9
MustafaTRKYLMZ <cana66331@gmail.com>
2024-03-25 12:55:32 +0100

    ex5 completed

--- 3-UsingAPIs/Week2/assignment/ex5-vscDebug.js
+++ 3-UsingAPIs/Week2/assignment/ex5-vscDebug.js
@@ -7,29 +7,31 @@ Use the VSCode Debugger to fix the bugs
 const fetch = require('node-fetch');
 
 async function getData(url) {
-  const response = await fetch(url);
-  return response.json();
+  try {
+    const response = await fetch(url);
+    return response.json();
+  } catch (err) {
+    console.error(`Something went wrong: ${err.message}`);
+  }
 }
 
 function renderLaureate({ knownName, birth, death }) {
   console.log(`\nName: ${knownName.en}`);
-  console.log(`Birth: ${birth.date}, ${birth.place.locationString}`);
-  console.log(`Death: ${death.date}, ${death.place.locationString}`);
+  console.log(`Birth: ${birth.date}, ${birth.place.locationString.en}`);
+  if (death) {
+    console.log(`Death: ${death.date}, ${death.place.locationString.en}`);
+  }
 }
 
-function renderLaureates(laureates) {
+function renderLaureates({ laureates }) {
   laureates.forEach(renderLaureate);
 }
 
 async function fetchAndRender() {
-  try {
-    const laureates = getData(
-      'http://api.nobelprize.org/2.0/laureates?birthCountry=Netherlands&format=json&csvLang=en'
-    );
-    renderLaureates(laureates);
-  } catch (err) {
-    console.error(`Something went wrong: ${err.message}`);
-  }
+  const laureates = await getData(
+    'http://api.nobelprize.org/2.0/laureates?birthCountry=Netherlands&format=json&csvLang=en'
+  );
+  renderLaureates(laureates);
 }
 
 fetchAndRender();
