02bd710f577e5428d79ce127e013b942b895800e
ervandogan12 <ervandogan12@gmail.com>
2024-03-26 17:09:48 +0100

    ahmet dogan using apis week-2  assignments

--- 3-UsingAPIs/Week2/assignment/ex4-diceRace.js
+++ 3-UsingAPIs/Week2/assignment/ex4-diceRace.js
@@ -10,23 +10,30 @@ Full description at: https://github.com/HackYourFuture/Assignments/blob/main/3-U
    resolves. Do you know why? Add your answer as a comment to the bottom of the 
    file.
 ------------------------------------------------------------------------------*/
-// ! Do not remove this line
 const rollDie = require('../../helpers/pokerDiceRoller');
 
 function rollDice() {
   const dice = [1, 2, 3, 4, 5];
-  // TODO complete this function; use Promise.race() and rollDie()
+  const promises = dice.map(() => rollDie());
+  return Promise.race(promises);
 }
 
 // Refactor this function to use async/await and try/catch
-function main() {
-  rollDice()
-    .then((results) => console.log('Resolved!', results))
-    .catch((error) => console.log('Rejected!', error.message));
+async function main() {
+  try {
+    const results = await rollDice();
+    console.log('Resolved!', results);
+  } catch (error) {
+    console.log('Rejected!', error.message);
+  }
 }
 
-// ! Do not change or remove the code below
 if (process.env.NODE_ENV !== 'test') {
   main();
 }
 module.exports = rollDice;
+
+//In the rollDice function, we are creating multiple promises for each die roll.
+//rollDice function returns a Promise that resolves or rejects as soon as one of the input Promises resolves or rejects.
+//The resolved value is the value of the first Promise in the input array to resolve, or the rejected reason of the first Promise to reject.
+//Because of that some dice continue rolling for some undetermined time after the promise returned by `Promise.race()
