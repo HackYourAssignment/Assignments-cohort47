f10a58eb2b09946b770b176dea7e6c2aedd70441
xrustik666 <kilorust@gmail.com>
2024-03-27 01:48:33 +0100

    ex3 - ex6 are done

--- 3-UsingAPIs/Week2/assignment/ex3-rollAnAce.js
+++ 3-UsingAPIs/Week2/assignment/ex3-rollAnAce.js
@@ -12,21 +12,23 @@ Full description at: https://github.com/HackYourFuture/Assignments/blob/main/3-U
 // ! Do not change or remove the next two lines
 const rollDie = require('../../helpers/pokerDiceRoller');
 
-function rollDieUntil(wantedValue) {
-  // TODO: rewrite this function using async/await
-  return rollDie().then((value) => {
-    if (value !== wantedValue) {
-      return rollDieUntil(wantedValue);
-    }
-    return value;
-  });
+async function rollDieUntil(wantedValue) {
+  const response = await rollDie();
+
+  if (response !== wantedValue) {
+    return rollDieUntil(wantedValue);
+  }
+  return value;
 }
 
-// TODO refactor this function to use try/catch
-function main() {
-  rollDieUntil('ACE')
-    .then((results) => console.log('Resolved!', results))
-    .catch((error) => console.log('Rejected!', error.message));
+async function main() {
+  try {
+    const results = await rollDieUntil('ACE');
+  
+    console.log('Resolved!', results);
+  } catch (error) {
+    console.log('Rejected!', error.message);
+  }
 }
 
 // ! Do not change or remove the code below
